1bd8f56ed894a2be0c641b49a6480ccc1a89a80f
Marcos Barros <108278189+Marcoswodbarros@users.noreply.github.com>
2025-05-23 14:25:09 -0300

    2_data-types

---
+++ 2_data-types/script.js
@@ -0,0 +1,16 @@
+// Tipos de dados
+
+// String - Conjunto de caracteres;
+let name = 'Marcos';
+
+// Number - Números inteiros ou com casa décimal;
+let age = 30;
+
+// Boolean - Verdadeiro ou falso;
+let isOld = false;
+
+// Undefined - Valor não atribuído;
+let lastName;
+
+// Null - Valor definido como "sem valor" (propositalmente).
+let color = null;
