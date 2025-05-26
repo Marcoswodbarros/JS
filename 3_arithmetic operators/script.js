26350a7d74a60b8ea3f32fe4cc1ee15ef961497a
Marcos Barros <108278189+Marcoswodbarros@users.noreply.github.com>
2025-05-26 14:00:38 -0300

    Arithmetic operators

---
+++ 3_arithmetic operators/script.js
@@ -0,0 +1,28 @@
+// Operators
+// Adição +
+let adicao = 2 + 1;
+
+// Subtração -
+let subtracao = 2 - 1;
+
+// Multiplicação *
+let multiplicacao = 2 * 1;
+
+// Divisão /
+let divisao = 2 / 1;
+
+// Módulo %
+let modulo = 2 % 1;
+
+// Exponenciação **
+let exponenciacao = 2 ** 2;
+
+// Incremento ++
+let incremento = 2;
+incremento++;
+
+// Descremento --
+let decremento = 2;
+decremento--;
+
+console.log(adicao, subtracao, multiplicacao, divisao, modulo, exponenciacao, incremento, decremento);
